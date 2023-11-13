0a155f5878c003c0db9b32a86be4f47a2a7d09d4
Siam11651 <siam11651@outlook.com>
2023-11-14 00:37:15 +0600

    kaaj kori

--- predictor.py
+++ predictor.py
@@ -3,7 +3,7 @@ from PIL import Image
 import numpy as np
 from argparse import ArgumentParser
 
-parser = ArgumentParser(prog="predictor", description="Detect whether an image contains a cat or not")
+parser = ArgumentParser(prog="predictor", description="Detect whether an image contains a cat or dog")
 
 parser.add_argument("--model", help="Set path to model", type=str, default="model/model.keras")
 parser.add_argument("--image", help="Set path to image", type=str)
--- predictor_lite.py
+++ predictor_lite.py
@@ -3,7 +3,7 @@ from PIL import Image
 import numpy as np
 from argparse import ArgumentParser
 
-parser = ArgumentParser(prog="predictor", description="Detect whether an image contains a cat or not")
+parser = ArgumentParser(prog="predictor", description="Detect whether an image contains a cat or dog")
 
 parser.add_argument("--model", help="Set path to model", type=str, default="model/model.tflite")
 parser.add_argument("--image", help="Set path to image", type=str)
@@ -22,4 +22,4 @@ interpreter.invoke()
 
 output = interpreter.get_tensor(output_details["index"])
 
-print(output) # means cat
+print(output) # 0 means cat
